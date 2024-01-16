a5120ad4b4ac4d631169084a9242174a7b1f3238
Balazs Kovacs <kobalazs@gmail.com>
2024-01-16 13:51:02 +0100

    Add antdesign & set up UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,22 @@
-import styles from './page.module.css'
+import { Alert, Button, Card, Flex, Input, Pagination, Space } from 'antd';
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Hello world
-        </p>
-      </div>
-    </main>
+    <Space direction="vertical" size="middle" style={{ display: 'flex', margin: 'auto', width: '80%' }}>
+      <h1>Movies</h1>
+      <Flex gap={20}>
+        <Input placeholder="Search input" />
+        <Button type="primary">Search</Button>
+      </Flex>
+      <Alert message="Lorem ipsum" type="success" showIcon />
+      <Flex gap={20}>
+        <Card title="Card title" style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Flex>
+      <Pagination defaultCurrent={1} total={50} />
+    </Space>
   )
 }
